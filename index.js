4f2ecd662fcdac51e450c97bc9eeeea1e97e49ec
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-16 23:51:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ if(string.toLowerCase() === string)
   return "I can't hear you!";
   else if(string.toUpperCase() === string)
   return "YES INDEED!";
-  else(string === "I love you, Grandma.");
+  else if(string === "I love you, Grandma.");
   return "I love you, too.";
 }
