3d5db42a0723d629359cc8d41a112d8b3050bce7
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-16 23:38:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 if(string.toLowerCase() === string)
   return "I can't hear you!";
-  if(string.toUpperCase() === string)
+esle if(string.toUpperCase() === string)
   return "YES INDEED!";
 }
